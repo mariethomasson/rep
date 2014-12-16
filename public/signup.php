8e25442fc52db48324fe8d5209d1cbf163cf3ea1
mariethomasson <marie.thomasson@gmail.com>
2014-12-16 09:32:04 +0100

    form completed

---
+++ public/signup.php
@@ -0,0 +1,33 @@
+<?php
+    if(isset ($_POST['spara'])) {
+        
+        echo "hej";
+        
+    } else
+
+
+?>
+<h1>Sign Up!</h1>
+<table>
+    <form action="signup.php" method="POST">
+        <tr>
+            <td>Username:</td>
+            <td><input type="text" name="username"></td>
+        </tr>
+        <tr>
+            <td>Email</td>
+            <td><input type="text" name="email"></td>
+        </tr>
+        <tr>
+            <td>Password:</td>
+            <td><input type="password" name="pass"></td>
+        </tr>
+        <tr>
+            <td>Re-password:</td>
+            <td><input type="password" name="rePass"></td>
+        </tr>
+        <tr>
+        <td><input type="submit" name="spara" value="signup"></td>
+        </tr>
+    </form>
+</table>
