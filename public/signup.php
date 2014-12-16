78ebbcad83ed9da18578e470f4f870b0e4fb6e29
mariethomasson <marie.thomasson@gmail.com>
2014-12-16 11:31:25 +0100

    Signup done

--- public/signup.php
+++ public/signup.php
@@ -1,30 +1,117 @@
 <?php
+    $db = new PDO("mysql:host=localhost;dbname=blog;", "root", "");
+
+    $userErr = $emailErr = $passErr = $rePassErr = "";
     if(isset ($_POST['spara'])) {
+        $class = $_POST['userRight'];
+        $user = trim($_POST['username']);
+        $email = trim($_POST['email']);
+        $pass = trim($_POST['pass']);
+        $rePAss = trim($_POST['rePass']);
         
-        echo "hej";
+        
+        if(!preg_match("/^[a-zA-Z ]*$/",$user)) {
+            $userErr = "Only letters and white space allowed";
+        }
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            $emailErr = "Invalid email format"; 
+        }
+        if(empty($user)) {
+            $userErr = "Username is required";
+        }
+        if(empty($email)) {
+            $emailErr = "Email is required";
+        }
+        if(empty($pass)) {
+            $passErr = "Password is required";
+        }
+        if($rePAss != $pass) {
+            $rePassErr = "Password and repass dont match";
+        }
+        
+        try{
+            $query = "SELECT username FROM users WHERE username = :user";
+            $ps = $db->prepare($query);
+            
+            $result = $ps->execute([
+                'user' => $user
+            ]);
+            
+            
+        }catch(Exception $err) {
+            echo $err;
+        }
+
+        $users = $ps->fetch(PDO::FETCH_ASSOC); //hämtar en rad
+        //$users $ps->fetchAll; //hämtar flera rader
+        
+        if($users) {
+            $userErr = "Username is taken. Please choose another username";
+        }
+        if(empty($userErr) && empty($emailErr) && empty($passErr)) {
+            
+            $pass = password_hash($pass, PASSWORD_BCRYPT);
+            
+            try{
+                $query = "INSERT INTO users (class_id, username, hashed_password, email) ";
+                $query .= "VALUES (:class, :user, :pass, :email) ";
+            
+            $ps = $db->prepare($query);
+            
+            $result = $ps->execute([
+                'class' => $class,
+                'user' => $user,
+                'pass' => $pass,
+                'email' => $email
+            ]);
+            
+            
+            }catch(Exception $err) {
+                echo $err;
+            }
+            
+            if($result) {
+                echo "Success";
+            }else {
+                echo "Failed";
+            }
+            
+        }
+        
         
     } else
 
 
+        
+        
 ?>
 <h1>Sign Up!</h1>
 <table>
     <form action="signup.php" method="POST">
         <tr>
+            <td>Usertype:</td>
+            <td><select name="userRight">
+                <option value="1">Student</option>
+                <option value="2">Teacher</option>
+                <option value="3">Admin</option>
+            </select></td>
+        </tr>
+        
+        <tr>
             <td>Username:</td>
-            <td><input type="text" name="username"></td>
+            <td><input type="text" name="username"><span><?php echo $userErr; ?></span></td>
         </tr>
         <tr>
             <td>Email</td>
-            <td><input type="text" name="email"></td>
+            <td><input type="text" name="email"><span><?php echo $emailErr; ?></span></td>
         </tr>
         <tr>
             <td>Password:</td>
-            <td><input type="password" name="pass"></td>
+            <td><input type="password" name="pass"><span><?php echo $passErr; ?></span></td>
         </tr>
         <tr>
             <td>Re-password:</td>
-            <td><input type="password" name="rePass"></td>
+            <td><input type="password" name="rePass"><span><?php echo $rePassErr; ?></span></td>
         </tr>
         <tr>
         <td><input type="submit" name="spara" value="signup"></td>
